940253ede81b21f2263714efc23a1dbc738155c8
Metalmerge <ermakovd06@gmail.com>
2023-08-31 16:23:36 -0400

    added more variation

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import time
+import random
 import pyautogui
 import threading
-import keyboard  # Added library for detecting keyboard input
+import keyboard
 
-# Global variable to control the loop
 running = True
 
 
 def click_on_screen(screen_number):
-    global running  # Access the global variable
+    global running
 
-    # Load the screenshot
     screenshot_path = f"screenshots/screen{screen_number}.png"
     target_location = pyautogui.locateOnScreen(screenshot_path)
 
@@ -18,7 +17,6 @@ def click_on_screen(screen_number):
         print(f"Screenshot {screen_number} not found on the screen.")
         return
 
-    # Get the center of the target location
     target_x, target_y, target_width, target_height = target_location
     target_center_x = target_x + target_width // 2
     target_center_y = target_y + target_height // 2
@@ -27,25 +25,22 @@ def click_on_screen(screen_number):
     pyautogui.moveTo(target_center_x, target_center_y)
     pyautogui.click()
 
-    # Check if the user pressed the escape key
     if keyboard.is_pressed("esc"):
         print("Escape key pressed. Stopping the program.")
         running = False
 
 
 def main():
-    # List of screen numbers to automate (adjust as needed)
     screens_to_automate = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
     for screen_number in screens_to_automate:
         if not running:
-            break  # Exit the loop if escape key is pressed
+            break
 
         click_on_screen(screen_number)
-        time.sleep(2)  # Add a delay to ensure the action is performed
+        time.sleep(random.uniform(3, 5))
 
 
 if __name__ == "__main__":
-    # Start a separate thread to detect the escape key
     threading.Thread(target=keyboard.wait, args=("esc",)).start()
     main()

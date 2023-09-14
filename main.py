abe652c165f7e76db3d5e4ab554d3aaf12b4284e
Metalmerge <ermakovd06@gmail.com>
2023-09-13 23:04:12 -0400

    refactored code and log

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # @Dimitry Ermakov
 # @09/09/2023
-
 import re
 import sys
 import threading
@@ -11,18 +10,26 @@ import keyboard
 import pyautogui
 import pytesseract
 
+# Constants
+EXIT_CODE = "-1"
+DEFAULT_PROMPT = "0"
+ESCAPE_KEY = "esc"
+MEDIUM_DELAY = 0.25
+LONG_DELAY = 0.77
+
+noted_date = "1/"
+
 pyautogui.FAILSAFE = True
-pyautogui.PAUSE = 0.75
+pyautogui.PAUSE = LONG_DELAY
+
 current_date = datetime.now()
 formatted_date = current_date.strftime("%-m/%Y")
-noted_date = "1/"
 
 
 def cord_click(x, y):
     pyautogui.moveTo(x, y)
     pyautogui.click()
-
-    if keyboard.is_pressed("esc"):
+    if keyboard.is_pressed(ESCAPE_KEY):
         print("Escape key pressed. Stopping the program.")
         sys.exit()
 
@@ -46,33 +53,48 @@ def contains_numbers(text):
     return any(char.isdigit() for char in text)
 
 
+def is_text_empty(text):
+    # Check if the text is None or consists only of whitespace characters
+    if text is None or len(text.strip()) == 0:
+        return True
+    else:
+        return False
+
+
+def contains_date(text):
+    # Regular expression pattern to match YYYY or MM/YYYY format dates
+    date_pattern = r"\b(\d{4}|\d{1,2}/\d{4})\b"
+
+    # Search for the pattern in the text
+    match = re.search(date_pattern, text)
+
+    # If a match is found, return True; otherwise, return False
+    if match:
+        return True
+    else:
+        return False
+
+
 def confirm():
     global noted_date
     time.sleep(0.75)
     cord_click(687, 250)
-    time.sleep(0.02)
     cord_click(535, 297)
-    time.sleep(0.02)
     cord_click(1022, 337)
-    time.sleep(0.02)
     cord_click(924, 526)
-    time.sleep(0.02)
     found_text = extract_text_from_coordinates(750, 1050, 2100, 1300)
-    if contains_numbers(found_text) is False and "year" in found_text == False:
+    if contains_date(found_text) is True and "year" in found_text is False:
         noted_date = pyautogui.prompt(text="", title="Noted Date?", default="1/")
-        time.sleep(0.02)
         cord_click(444, 444)
-
     time.sleep(1)
     cord_click(1035, 617)
-    pyautogui.press("enter")
-    time.sleep(0.02)
-    pyautogui.press("enter")
-    time.sleep(0.02)
+    if is_text_empty(found_text) == False:
+        pyautogui.press("enter")
+        pyautogui.press("enter")
     keyboard.write("Note: Not Researched - DE")
-    time.sleep(0.25)
+    time.sleep(MEDIUM_DELAY)
     cord_click(857, 826)
-    time.sleep(0.02)
+    time.sleep(MEDIUM_DELAY)
     cord_click(912, 828)  # save button
     # cord_click(1015, 825)  # cancel button
 
@@ -81,13 +103,9 @@ def decline(num):
     # form
     time.sleep(2)
     cord_click(687, 252)
-    time.sleep(0.02)
     cord_click(523, 338)
-    time.sleep(0.02)
     cord_click(1023, 338)
-    time.sleep(0.02)
     cord_click(924, 526)
-    time.sleep(0.02)
     cord_click(1035, 635)
     pyautogui.press("enter")
     pyautogui.press("enter")
@@ -98,11 +116,11 @@ def decline(num):
     cord_click(912, 828)  # save button
     # cord_click(1015, 825)  # cancel button
     if num <= 3:
-        return "0"
+        return DEFAULT_PROMPT
     elif num > 3:
         time.sleep(0.5)
         end = pyautogui.prompt(
-            text="", title="More duplicates?, 1 = yes, 0 = no", default="0"
+            text="", title="More duplicates?, 1 = yes, 0 = no", default=DEFAULT_PROMPT
         )
         cord_click(194, 664)  # safe corner
         time.sleep(0.5)
@@ -112,16 +130,13 @@ def decline(num):
 def deceased_form():
     global noted_date
     cord_click(716, 458)  # date
-    time.sleep(0.02)
     if noted_date == "1/":
         keyboard.write(formatted_date)
     elif noted_date != "1/":
         keyboard.write(noted_date)
-    time.sleep(0.02)
     cord_click(857, 514)
-    time.sleep(0.02)
     cord_click(772, 562)  # reason
-    time.sleep(0.02)
+    time.sleep(MEDIUM_DELAY)
     cord_click(736, 571)  # save button
     # cord_click(834, 579)  # cancel button
 
@@ -141,48 +156,40 @@ def move_to_communications():
     cord_click(73, 370)  # consituents
     time.sleep(6)
     cord_click(497, 826)  # communications
-    time.sleep(0.02)
 
 
 def opt_out_form():
-    time.sleep(0.25)
+    time.sleep(MEDIUM_DELAY)
     cord_click(703, 442)
     time.sleep(0.5)
     keyboard.write("Imprimis")
     time.sleep(0.5)
     cord_click(572, 442)  # wait
     time.sleep(0.5)
-
     cord_click(850, 505)  # date button
-    time.sleep(0.02)
     cord_click(748, 686)  # today
-
-    time.sleep(0.02)
     cord_click(850, 476)  # tab
-    time.sleep(0.02)
     cord_click(733, 515)  # opt out
-    time.sleep(0.02)
     # cord_click(698, 642)
     # time.sleep(1)
     cord_click(849, 637)  # tab
-    time.sleep(0.25)
+    time.sleep(MEDIUM_DELAY)
     cord_click(698, 706)  # deceased press
-    time.sleep(0.02)
     cord_click(636, 642)
-    time.sleep(0.02)
-    for _ in range(0, 3):
-        pyautogui.press("down")
-
-    time.sleep(0.02)
+    down_command(3)
     cord_click(737, 820)  # save
     # cord_click(272, 225)  # click on the search bar
 
 
+def down_command(num):
+    for _ in range(0, num):
+        pyautogui.press("down")
+
+
 def interactions_section(num):
     time.sleep(5)
-    for _ in range(0, 6):
-        pyautogui.press("down")
-    time.sleep(0.25)
+    down_command(6)
+    time.sleep(MEDIUM_DELAY)
     click_on_first_interaction()
 
     if num == 1:
@@ -194,9 +201,9 @@ def interactions_section(num):
         time.sleep(1)
     elif num == 2:
         confirm()
-        time.sleep(1.5)
+        time.sleep(2)
         cord_click(415, 815)  # click on the second pending
-        time.sleep(1)
+        time.sleep(1.5)
         cord_click(298, 837)  # click on second edit button
         decline(num)
         time.sleep(1.75)
@@ -212,8 +219,7 @@ def interactions_section(num):
         cord_click(298, 837)  # click on second edit button
         decline(num)
         time.sleep(1)
-        for _ in range(0, 2):
-            pyautogui.press("down")
+        down_command(2)
         time.sleep(1)
         cord_click(418, 756)  # click on the third pending
         time.sleep(1)
@@ -232,15 +238,15 @@ def interactions_section(num):
             pyautogui.prompt(
                 text="",
                 title="Enter when you are at the decline form",
-                default="0",
+                default=DEFAULT_PROMPT,
             )
             cord_click(857, 826)
-            if decline(num) == "0":
+            if decline(num) == DEFAULT_PROMPT:
                 duplicates = False
             pyautogui.prompt(
                 text="",
                 title="Enter when you are at the deceased form",
-                default="0",
+                default=DEFAULT_PROMPT,
             )
             cord_click(639, 515)  # wait
             time.sleep(1)
@@ -253,10 +259,7 @@ def extract_digits_from_text(text):
 def find_year(text):
     # Define a regular expression pattern to match a year (e.g., 4 digits)
     pattern = r"\b\d{4}\b"
-
-    # Search for the year pattern in the text
     match = re.search(pattern, text)
-
     if match:
         year = match.group()
         return year
@@ -265,11 +268,14 @@ def find_year(text):
 
 
 def main():
-    job = pyautogui.prompt(text="", title="Enter the Task, -1 to quit", default="0")
-    if job == "-1":
+    job = pyautogui.prompt(
+        text="", title="Enter the Task, -1 to quit", default=DEFAULT_PROMPT
+    )
+    if job == EXIT_CODE:
         sys.exit()
     cord_click(271, 173)
-    while job != "-1" and keyboard.is_pressed("esc") is False:
+    while job != EXIT_CODE and keyboard.is_pressed(ESCAPE_KEY) is False:
+        start_time = time.time()
         get_to_dead_page()
         time.sleep(5)
         num = int(
@@ -277,21 +283,24 @@ def main():
                 extract_text_from_coordinates(420, 1350, 620, 1400)
             )
         )
-        time.sleep(0.25)
+        time.sleep(MEDIUM_DELAY)
         cord_click(262, 691)  # interactions button
         interactions_section(num)
         deceased_form()
         time.sleep(3)
         move_to_communications()
-
-        for _ in range(0, 2):
-            pyautogui.press("down")
-        time.sleep(0.25)
+        down_command(2)
+        time.sleep(MEDIUM_DELAY)
         cord_click(382, 822)  # click on add for communications
         opt_out_form()
-        time.sleep(4)
+        time.sleep(5)
+        end_time = time.time()
+        duration = end_time - start_time
+        log_file = "program_log.txt"
+        with open(log_file, "a") as f:
+            f.write(f"Program execution time: {duration:.2f} seconds\n")
 
 
 if __name__ == "__main__":
-    threading.Thread(target=keyboard.wait, args=("esc",)).start()
+    threading.Thread(target=keyboard.wait, args=(ESCAPE_KEY,)).start()
     main()

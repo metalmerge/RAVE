c7e16ae5c30e34a5ad8ad491909d369e23cf12bf
Metalmerge <ermakovd06@gmail.com>
2023-09-23 14:17:13 -0400

    extra

--- automata.py
+++ automata.py
@@ -3,6 +3,10 @@ import time
 import keyboard
 from datetime import datetime
 
+pyautogui.FAILSAFE = True
+DELAY = 1
+pyautogui.PAUSE = DELAY
+
 
 def down_command(num):
     for _ in range(0, num):
@@ -10,23 +14,21 @@ def down_command(num):
 
 
 def find_and_click_image(image_filename, biasx, biasy):
-    time.sleep(0.1)
     try:
         box = None
         x_scale = 1440 / 2880
         y_scale = 900 / 1800
         while box is None:
             box = pyautogui.locateOnScreen(image_filename, confidence=0.9)
-            time.sleep(0.5)
+            time.sleep(DELAY)
             print("Searching for image: " + image_filename)
 
-        # print(box)
         x, y, width, height = box
 
         # screenshot = pyautogui.screenshot()
         # found_image_screenshot = screenshot.crop((x, y, x + width, y + height))
-        # found_image_screenshot.save("found_image.png")
         # found_image_screenshot.show()
+        # found_image_screenshot.save(image_filename)
 
         x = box.left * x_scale
         y = box.top * y_scale
@@ -43,8 +45,18 @@ def cord_click(x, y):
     pyautogui.click()
 
 
-find_and_click_image("target/source_tab_down.png", 0, 0)
-find_and_click_image("target/communication_from.png", 0, 0)
-
-
-# find_and_click_image("test.png", 0, 0)
+global full_date
+find_and_click_image("target/solicit_code.png", 0, 0)
+keyboard.write("Imprimis")
+find_and_click_image("target/opt_out_comments.png", 0, 0)
+find_and_click_image("target/start_date.png", 0, 0)
+# find_and_click_image("target/today.png", 0, 0)
+keyboard.write("9/23/2023")
+find_and_click_image("target/prefernce_tab_down.png", 0, 0)
+find_and_click_image("target/opt_out.png", 0, 0)
+find_and_click_image("target/imprintis_source.png", 0, 0)
+keyboard.write("Deceased")
+find_and_click_image("target/source_evidence.png", 0, 0)
+down_command(3)
+# find_and_click_image("target/save.png", 0, 0)
+find_and_click_image("target/cancel.png", 0, 0)  # cancel button

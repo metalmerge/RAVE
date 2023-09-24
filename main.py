807e68a214d0dc3753fbe0a6e310f9820a792a87
Metalmerge <ermakovd06@gmail.com>
2023-09-23 22:50:29 -0400

    60 seconds

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 # @Dimitry Ermakov
 # @09/23/2023
-import re
 import time
 from datetime import datetime
+
 import keyboard
 import pyautogui
 import pytesseract
-import os
+from mac_notifications import client
 
 # Interactions: 2 = 79.14 bot; 75.66 no copy pasting, experienced, fast as possible human
-# Interactions: 1 = 55.25 bot; 51.08 no copy pasting, experienced, fast as possible human
+# Interactions: 1 = 65.64 bot; 51.08 no copy pasting, experienced, fast as possible human
 
 # TODO get ride of needing biasX and biasY, fix extract_text_from_coordinates hard coded coordinates
-# TODO change more find_and_click_image to tabbing
 # text_comment scanner did not work
-# prompt for screen size
 # reform num > 3
 # get sound for notifications
-# tab more
 # up and down commands to relate to screen size
 
 DEFAULT_PROMPT = "0"
@@ -31,14 +28,11 @@ x_scale = 1
 y_scale = 1
 # x_scale = 1440 / 2880
 # y_scale = 900 / 1800
-
 current_date = datetime.now()
 formatted_date = current_date.strftime("%-m/%Y")
 full_date = current_date.strftime("%-m/%-d/%Y")
 PRIMIS = "target/receives_imprimis.png"
 CRM = "target/blackbaud_CRM.png"
-
-import time
 
 
 def find_and_click_image(image_filename, biasx, biasy):
@@ -49,20 +43,17 @@ def find_and_click_image(image_filename, biasx, biasy):
             box = pyautogui.locateOnScreen(image_filename, confidence=0.9)
             time.sleep(DELAY)
             print("Searching for image: " + image_filename)
-
             attempts += 1
             if attempts >= MAX_ATTEMPTS:
-                os.system(
-                    "osascript -e 'display notification \"Image not found after "
-                    + str(MAX_ATTEMPTS)
-                    + ' attempts" with title "Failed to find Image" sound name "Glass"\''
+                client.create_notification(
+                    title="Error",
+                    subtitle="Could not find image",
                 )
-
                 break
 
         x, y, width, height = box
-        x = box.left * 0.5 + width / 4 + biasx
-        y = box.top * 0.5 + height / 4 + biasy
+        x = box.left / 2 + width / 4 + biasx
+        y = box.top / 2 + height / 4 + biasy
         if (
             image_filename != PRIMIS
             and image_filename != "target/receives_imprimis.png"
@@ -90,10 +81,12 @@ def down_command(num):
         pyautogui.press("down")
 
 
-def contains_digits(text):
-    pattern = r"\d"
-    match = re.search(pattern, text)
-    if match:
+def extract_digits_from_text(text):
+    return "".join(filter(str.isdigit, text))
+
+
+def is_text_empty(text):
+    if text is None or len(text.strip()) == 0:
         return True
     else:
         return False
@@ -105,60 +98,6 @@ def get_to_dead_page():
     find_and_click_image("target/name.png", 0, 25)
 
 
-def click_on_top_interaction(num):
-    STATUS = "target/status_alone.png"
-    find_and_click_image(STATUS, 0, (num * 30))
-    find_and_click_image("target/edit_interaction.png", 0, 0)
-
-
-def interactions_section(num):
-    global CRM, PRIMIS
-    OWNER = "target/wait_for_owner.png"
-    find_and_click_image("target/interactions.png", 0, 0)
-    find_and_click_image(PRIMIS, 0, 0)
-    down_command(num * 2 + 8)
-    click_on_top_interaction(1)
-    confirm()
-    if num == 2:
-        find_and_click_image(OWNER, 0, 0)
-        # down_command(num)
-        click_on_top_interaction(num)
-        decline(num)
-
-    elif num == 3:
-        find_and_click_image(OWNER, 0, 0)
-        # down_command(num - 1)
-        click_on_top_interaction(num - 1)  # starts at 2 always
-        decline(num)
-        find_and_click_image(OWNER, 0, 0)
-        # down_command(num - 2)
-        click_on_top_interaction(num)
-        decline(num)
-
-    elif num > 3:
-        confirm()
-
-        duplicates = True
-        while duplicates:
-            pyautogui.prompt(
-                text="",
-                title="Enter when you are at the decline form",
-                default=DEFAULT_PROMPT,
-            )
-            find_and_click_image(CRM, 0, 0)
-            if decline(num) == DEFAULT_PROMPT:
-                duplicates = False
-    up_command(num * 3)
-    get_to_mark_deceased()
-
-
-def is_text_empty(text):
-    if text is None or len(text.strip()) == 0:
-        return True
-    else:
-        return False
-
-
 def confirm():
     global noted_date, initials, full_date
 
@@ -168,7 +107,7 @@ def confirm():
     keyboard.write(full_date)
     found_text = extract_text_from_coordinates(750, 1050, 2100, 1300)
     if (
-        contains_digits(found_text) is True
+        extract_digits_from_text(found_text) != ""
         and "year" not in found_text
         and "month" not in found_text
     ):
@@ -208,6 +147,52 @@ def decline(num):
     return end
 
 
+def click_on_top_interaction(num):
+    STATUS = "target/status_alone.png"
+    find_and_click_image(STATUS, 0, (num * 30))
+    find_and_click_image("target/edit_interaction.png", 0, 0)
+
+
+def interactions_section(num):
+    global CRM, PRIMIS
+    OWNER = "target/wait_for_owner.png"
+    find_and_click_image("target/interactions.png", 0, 0)
+    down_command(num * 2 + 8)
+    find_and_click_image(PRIMIS, 0, 0)
+    click_on_top_interaction(1)
+    confirm()
+    if num == 2:
+        find_and_click_image(OWNER, 0, 0)
+        # down_command(num)
+        click_on_top_interaction(num)
+        decline(num)
+
+    elif num == 3:
+        find_and_click_image(OWNER, 0, 0)
+        # down_command(num - 1)
+        click_on_top_interaction(num - 1)  # starts at 2 always
+        decline(num)
+        find_and_click_image(OWNER, 0, 0)
+        # down_command(num - 2)
+        click_on_top_interaction(num)
+        decline(num)
+
+    elif num > 3:
+        confirm()
+        duplicates = True
+        while duplicates:
+            pyautogui.prompt(
+                text="",
+                title="Enter when you are at the decline form",
+                default=DEFAULT_PROMPT,
+            )
+            find_and_click_image(CRM, 0, 0)
+            if decline(num) == DEFAULT_PROMPT:
+                duplicates = False
+    up_command(num * 3)
+    get_to_mark_deceased()
+
+
 def deceased_form():
     global noted_date
     find_and_click_image("target/deceased_date.png", 0, 0)
@@ -234,6 +219,7 @@ def move_to_communications():
     find_and_click_image("target/constitute.png", 0, 0)
     find_and_click_image(PRIMIS, 0, 0)
     down_command(3)
+    find_and_click_image(PRIMIS, 0, 0)
     find_and_click_image("target/communications.png", 0, 0)
     find_and_click_image("target/add.png", 0, 0)
 
@@ -255,6 +241,7 @@ def opt_out_form():
 
 
 def get_to_mark_deceased():
+    find_and_click_image(PRIMIS, 0, 0)
     find_and_click_image("target/personal_info.png", 0, 0)
     find_and_click_image("target/marked_deceased.png", 0, 0)
 
@@ -277,10 +264,6 @@ def interactions_num_finder():
             time.sleep(DELAY)
             continue
     return num
-
-
-def extract_digits_from_text(text):
-    return "".join(filter(str.isdigit, text))
 
 
 def end_time_recording(start_time):

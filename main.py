b2fa2d6d063ff693ee2fda7e42021253fdb85daf
Metalmerge <ermakovd06@gmail.com>
2023-09-30 17:37:48 -0400

    fix if and added scale, and search

--- main.py
+++ main.py
@@ -9,21 +9,21 @@ import pytesseract
 import os
 
 # Interactions: 2 = 79.14 bot; 75.66 no copy pasting, experienced, fast as possible human
-# Interactions: 1 = 60.42 bot; 51.08 no copy pasting, experienced, fast as possible human
+# Interactions: 1 = 50.66 bot; 51.08 no copy pasting, experienced, fast as possible human
 
 # TODO
-# x_scale = 1440 / 2880
-# y_scale = 900 / 1800
+# Protential improvements:
+#   - Find a better way to know the number of interactions than using extract_text_from_coordinates
+#   - Working sound notification for macOS and Windows
+#   - Use mss to take screenshots instead of pyautogui
+#   - Find workaround to recieves imprints and waits
+
+# original_x_scale = 1440 / 2880
+# original_y_scale = 900 / 1800
 # reform num > 3
-# up and down commands to relate to screen size
-# remove extract from text(Sep 30)
-# add sound alert for prompt waiting too long and notification(Sep 30)
-# if can't find, downcommand?(Sep 30)
-# make sure commands and control are accounted for
-# time how long it takes for each action, find workaround to recieves imprints or waits(Sep 30)
-# Conditions for selling(Sep 30)
-# pythpm mss screenshot(Sep 30)
-# Scalability, need 1 for 3 devices
+# make sure commands and control are accounted for based on type of computer
+
+# TODO rework readme
 
 DEFAULT_PROMPT = "0"
 initials = "DE"
@@ -36,6 +36,7 @@ DELAY = 0.01
 MAX_ATTEMPTS = round(1.25 / (DELAY * 5))
 x_scale = 1
 y_scale = 1
+COM_NUM = 1
 pyautogui.FAILSAFE = True
 pyautogui.PAUSE = DELAY
 current_date = datetime.now()
@@ -46,10 +47,9 @@ cutOffTopY = 0
 cutOffBottomY = 900
 
 
-def find_and_click_image_with_search(image_filename, continuous_search, up_or_down):
+def find_and_click_image_with_search(image_filename, biasx, biasy, up_or_down):
     global cutOffTopY, DELAY, MAX_ATTEMPTS, x_scale, y_scale, cutOffBottomY
     box = None
-    attempts = 0
     # print("Searching for image: " + image_filename)
     while box is None:
         box = pyautogui.locateOnScreen(
@@ -63,28 +63,14 @@ def find_and_click_image_with_search(image_filename, continuous_search, up_or_do
             ),
         )
         time.sleep(DELAY * 5)
-        attempts += 1
-        if attempts == 5 and continuous_search is True:
-            pyautogui.press("down")
-            attempts = 0
-        if attempts >= MAX_ATTEMPTS:
-            if os.name == "posix":  # macOS # TODO untested
-                os.system(
-                    f'osascript -e \'display notification "Could not find image {image_filename}" with title "Error" sound name "Glass"\''
-                )
-        elif os.name == "nt":  # Windows # TODO untested
-            os.system(
-                f'powershell -command "New-BurntToastNotification -Text "Could not find image {image_filename}" -AppLogo '
-                + '"'
-                + os.getcwd()
-                + "/target/chrome.png"
-                + ")"
-                + '"'
-            )
+        if box is None:
+            pyautogui.press(up_or_down)
+            pyautogui.press(up_or_down)
+            time.sleep(1)  # TODO
 
     x, y, width, height = box
-    x = box.left / 2 + width / 4
-    y = box.top / 2 + height / 4
+    x = box.left / 2 + width / 4 + biasx
+    y = box.top / 2 + height / 4 + biasy
     if (  # TODO find a way to remove
         image_filename != PRIMIS
         and image_filename != EDUCATION
@@ -216,8 +202,13 @@ def is_text_empty(text):
 
 
 def get_to_dead_page():
+    global COM_NUM
     find_and_click_image("target/constituents.png")
     find_and_click_image("target/updates.png")
+    if COM_NUM == 2:  # untested
+        find_and_click_image("target/third_page.png")
+    if COM_NUM == 3:
+        find_and_click_image("target/fifth_page.png")
     find_and_click_image_with_bias("target/name.png", 0, round(25 * y_scale))
 
 
@@ -247,18 +238,15 @@ def interactions_num_finder():
 
 
 def click_on_top_interaction(num):
-    find_and_click_image_with_bias(
-        "target/status_alone.png", 0, round(num * 30 * y_scale)
+    find_and_click_image_with_search(
+        "target/status_alone.png", 0, round(num * 30 * y_scale), "down"
     )
-    find_and_click_image("target/edit_interaction.png")
+    find_and_click_image_with_search("target/edit_interaction.png", 0, 0, "down")
 
 
 def interactions_section(num):
     global PRIMIS, LOAD_OWNER_WAIT
     find_and_click_image("target/interactions.png")
-    find_and_click_image(PRIMIS)  # TODO find a way to remove
-    for _ in range(0, num + 8):
-        pyautogui.press("down")
     click_on_top_interaction(1)
     confirm()
     if num == 2:
@@ -272,7 +260,6 @@ def interactions_section(num):
         find_and_click_image(LOAD_OWNER_WAIT)
         click_on_top_interaction(num)
         decline(num)
-
     elif num > 3:
         confirm()
         duplicates = True
@@ -285,10 +272,8 @@ def interactions_section(num):
             cord_click(CRM_cords)
             if decline(num) == DEFAULT_PROMPT:
                 duplicates = False
-    for _ in range(0, num * 3 + 1):
-        pyautogui.press("up")
-    find_and_click_image(PRIMIS)  # TODO find a way to remove
-    find_and_click_image("target/personal_info.png")
+    find_and_click_image(PRIMIS)
+    find_and_click_image_with_search("target/personal_info.png", 0, 0, "up")
     find_and_click_image("target/marked_deceased.png")
 
 
@@ -307,21 +292,23 @@ def confirm():
     found_text = pyperclip.paste()
     print(found_text)
     if (
-        extract_digits_from_text(found_text) != ""
-        or "year" in found_text
-        or "month" in found_text
-        or "January" in found_text
-        or "February" in found_text
-        or "March" in found_text
-        or "April" in found_text
-        or "May" in found_text
-        or "June" in found_text
-        or "July" in found_text
-        or "August" in found_text
-        or "September" in found_text
-        or "October" in found_text
-        or "November" in found_text
-        or "December" in found_text
+        (
+            extract_digits_from_text(found_text) != ""
+            or "year" in found_text
+            or "month" in found_text
+            or "January" in found_text
+            or "February" in found_text
+            or "March" in found_text
+            or "April" in found_text
+            or "May" in found_text
+            or "June" in found_text
+            or "July" in found_text
+            or "August" in found_text
+            or "September" in found_text
+            or "October" in found_text
+            or "November" in found_text
+            or "December" in found_text
+        )
         and "id=" not in found_text
         and "batch" not in found_text
     ):
@@ -349,21 +336,23 @@ def decline(num):
     found_text = pyperclip.paste()
     print(found_text)
     if (
-        extract_digits_from_text(found_text) != ""
-        or "year" in found_text
-        or "month" in found_text
-        or "January" in found_text
-        or "February" in found_text
-        or "March" in found_text
-        or "April" in found_text
-        or "May" in found_text
-        or "June" in found_text
-        or "July" in found_text
-        or "August" in found_text
-        or "September" in found_text
-        or "October" in found_text
-        or "November" in found_text
-        or "December" in found_text
+        (
+            extract_digits_from_text(found_text) != ""
+            or "year" in found_text
+            or "month" in found_text
+            or "January" in found_text
+            or "February" in found_text
+            or "March" in found_text
+            or "April" in found_text
+            or "May" in found_text
+            or "June" in found_text
+            or "July" in found_text
+            or "August" in found_text
+            or "September" in found_text
+            or "October" in found_text
+            or "November" in found_text
+            or "December" in found_text
+        )
         and "id=" not in found_text
         and "batch" not in found_text
     ):
@@ -405,9 +394,7 @@ def move_to_communications():
     find_and_click_image("target/constitute.png")
     find_and_click_image(PRIMIS)  # TODO find a way to remove
     find_and_click_image("target/communications.png")
-    for _ in range(0, 2):
-        pyautogui.press("down")
-    find_and_click_image("target/add.png")
+    find_and_click_image_with_search("target/add.png", 0, 0, "down")
 
 
 def opt_out_form():
@@ -477,15 +464,16 @@ def cutoff_section_of_screen(image_filename):
 
 
 def main():
-    global initials, cutOffTopY, x_scale, y_scale, CRM_cords, cutOffBottomY, EDUCATION
+    global initials, cutOffTopY, x_scale, y_scale, CRM_cords, cutOffBottomY, EDUCATION, COM_NUM
     input_str = pyautogui.prompt(
         text="Enter Initials and screen , -1 to quit",
         title="Enter Initials and screen , -1 to quit",
-        default="DE, 1440, 900",
+        default="DE, 1440, 900, 1",
     )
-    initials, x_scale, y_scale = input_str.strip().split(",")
+    initials, x_scale, y_scale, computer_number = input_str.strip().split(",")
     x_scale = int(x_scale.strip()) / 1440
     y_scale = int(y_scale.strip())
+    COM_NUM = int(computer_number.strip())
     cutOffBottomY = y_scale
     y_scale /= 900
     cutOffTopY, CRM_cords = cutoff_section_of_screen("target/blackbaud_CRM.png")

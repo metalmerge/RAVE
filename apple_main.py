f318cca395ef556bb1e10f38f12a4ac6c375c4fa
Metalmerge <ermakovd06@gmail.com>
2023-10-04 00:04:02 -0400

    refactored var names apple

--- apple_main.py
+++ apple_main.py
@@ -25,126 +25,62 @@ import os
 
 # TODO rework readme
 
-DEFAULT_PROMPT = "0"
 initials = "DE"
 noted_date = "1/"
+delay = 0.01
+x_scale = 1
+y_scale = 1
+COM_NUM = 1
+confidence = 0.9
+CRM_cords = (0, 0)
+cutOffTopY = 0
+cutOffBottomY = 900
+pyautogui.FAILSAFE = True
+pyautogui.PAUSE = delay
+CURRENT_DATE = datetime.now()
+FORMATTED_DATE = CURRENT_DATE.strftime("%-m/%Y")
+FULL_DATE = CURRENT_DATE.strftime("%-m/%-d/%Y")
+DEFAULT_PROMPT = "0"
 PRIMIS = "appletarget/receives_imprimis.png"
 EDUCATION = "appletarget/education.png"
 LOAD_OPT_OUT_WAIT = "appletarget/wait_for_load_opt_out.png"
 LOAD_OWNER_WAIT = "appletarget/wait_for_load_owner.png"
-DELAY = 0.01
-MAX_ATTEMPTS = round(1.25 / (DELAY * 5))
-x_scale = 1
-y_scale = 1
-COM_NUM = 1
-CONFIDENCE = 0.9
-pyautogui.FAILSAFE = True
-pyautogui.PAUSE = DELAY
-current_date = datetime.now()
-formatted_date = current_date.strftime("%-m/%Y")
-full_date = current_date.strftime("%-m/%-d/%Y")
-
-CRM_cords = (0, 0)
-cutOffTopY = 0
-cutOffBottomY = 900
+MAX_ATTEMPTS = round(1.25 / (delay * 5))
 
 
-def find_and_click_image_with_search(image_filename, biasx, biasy, up_or_down):
-    global cutOffTopY, DELAY, MAX_ATTEMPTS, x_scale, y_scale, cutOffBottomY, CONFIDENCE
-    box = None
-    # print("Searching for image: " + image_filename)
-    while box is None:
-        box = pyautogui.locateOnScreen(
-            image_filename,
-            confidence=CONFIDENCE,
-            region=(
-                0,
-                cutOffTopY,
-                round(2880 * x_scale),
-                round(cutOffBottomY * 2 * y_scale),  # theocratically this works
-            ),
-        )
-        time.sleep(DELAY * 5)
-        if box is None:  # skroll
-            pyautogui.press(up_or_down)
-            pyautogui.press(up_or_down)
-            pyautogui.press(up_or_down)
-            time.sleep(DELAY * 5)
-
-    x, y, width, height = box
-    x = box.left / 2 + width / 4 + biasx
-    y = box.top / 2 + height / 4 + biasy
-    if (  # TODO find a way to remove
-        image_filename != PRIMIS
-        and image_filename != EDUCATION
-        and image_filename != LOAD_OPT_OUT_WAIT
-        and image_filename != LOAD_OWNER_WAIT
-    ):
-        cord_click((x, y))
-
-
-def find_and_click_image_with_bias(image_filename, biasx, biasy):
-    global cutOffTopY, DELAY, MAX_ATTEMPTS, x_scale, y_scale, cutOffBottomY, CONFIDENCE
+def find_and_click_image(image_filename, biasx=0, biasy=0, up_or_down=None):
     box = None
     attempts = 0
-    # print("Searching for image: " + image_filename)
+
     while box is None:
         box = pyautogui.locateOnScreen(
             image_filename,
-            confidence=CONFIDENCE,
+            confidence=confidence,
             region=(
                 0,
                 cutOffTopY,
                 round(2880 * x_scale),
-                round(cutOffBottomY * 2 * y_scale),  # theocratically this works
+                round(cutOffBottomY * 2 * y_scale),
             ),
         )
-        time.sleep(DELAY * 5)
+        time.sleep(delay * 5)
         attempts += 1
         if attempts >= MAX_ATTEMPTS:
             os.system(
                 f'osascript -e \'display notification "Could not find image {image_filename}" with title "Error" sound name "Glass"\''
             )
 
+        if box is None and up_or_down is not None:
+            pyautogui.press(up_or_down)
+            pyautogui.press(up_or_down)
+            pyautogui.press(up_or_down)
+            time.sleep(delay * 5)
+
     x, y, width, height = box
     x = box.left / 2 + width / 4 + biasx
     y = box.top / 2 + height / 4 + biasy
-    if (  # TODO find a way to remove
-        image_filename != PRIMIS
-        and image_filename != EDUCATION
-        and image_filename != LOAD_OPT_OUT_WAIT
-        and image_filename != LOAD_OWNER_WAIT
-    ):
-        cord_click((x, y))
 
-
-def find_and_click_image(image_filename):
-    global cutOffTopY, DELAY, MAX_ATTEMPTS, x_scale, y_scale, cutOffBottomY, CONFIDENCE
-    box = None
-    attempts = 0
-    # print("Searching for image: " + image_filename)
-    while box is None:
-        box = pyautogui.locateOnScreen(
-            image_filename,
-            confidence=CONFIDENCE,
-            region=(
-                0,
-                cutOffTopY,
-                round(2880 * x_scale),
-                round(cutOffBottomY * 2 * y_scale),  # theocratically this works
-            ),
-        )
-        time.sleep(DELAY * 5)
-        attempts += 1
-        if attempts >= MAX_ATTEMPTS:
-            os.system(
-                f'osascript -e \'display notification "Could not find image {image_filename}" with title "Error" sound name "Glass"\''
-            )
-
-    x, y, width, height = box
-    x = box.left / 2 + width / 4
-    y = box.top / 2 + height / 4
-    if (  # TODO find a way to remove
+    if (
         image_filename != PRIMIS
         and image_filename != EDUCATION
         and image_filename != LOAD_OPT_OUT_WAIT
@@ -167,9 +103,8 @@ def cord_click(cords):
     pyautogui.click()
 
 
-def tab_command(num, delay):
-    for _ in range(0, num):
-        time.sleep(delay)
+def tab_command(number_of_interactions):
+    for _ in range(0, number_of_interactions):
         pyautogui.press("tab")
 
 
@@ -192,11 +127,11 @@ def get_to_dead_page():
         find_and_click_image("appletarget/third_page.png")
     if COM_NUM == 3:
         find_and_click_image("appletarget/fifth_page.png")
-    find_and_click_image_with_bias("appletarget/name.png", 0, round(25 * y_scale))
+    find_and_click_image("appletarget/name.png", 0, round(25 * y_scale))
 
 
 def interactions_num_finder():
-    global DELAY
+    global delay
     while True:
         pretext = "Interactions: "
         try:
@@ -210,48 +145,56 @@ def interactions_num_finder():
                 # Extract the number following "Interactions:"
                 num_index = text.index(pretext) + len(pretext)
                 num_text = text[num_index:].strip()
-                num = int(extract_digits_from_text(num_text))
+                number_of_interactions = int(extract_digits_from_text(num_text))
                 break
             else:
-                time.sleep(DELAY)
+                time.sleep(delay)
                 continue
         except ValueError:
             continue
-    return num
+    return number_of_interactions
 
 
-def click_on_top_interaction(num):
-    find_and_click_image_with_search(
-        "appletarget/status_alone.png", 0, round(num * 30 * y_scale), "down"
+def click_on_top_interaction(number_of_interactions):
+    find_and_click_image(
+        "appletarget/status_alone.png",
+        0,
+        round(number_of_interactions * 30 * y_scale),
+        "down",
     )
-    find_and_click_image_with_search("appletarget/edit_interaction.png", 0, 0, "down")
+    find_and_click_image("appletarget/edit_interaction.png", 0, 0, "down")
 
 
-def interactions_section(num):
+def interactions_section(number_of_interactions):
     global PRIMIS, LOAD_OWNER_WAIT
     find_and_click_image("appletarget/interactions.png")
     click_on_top_interaction(1)
-    confirm()
-    if num > 1:  # untested
-        for i in range(2, num):
+    process_application()
+    if number_of_interactions > 1:  # untested
+        for i in range(2, number_of_interactions):
             find_and_click_image(LOAD_OWNER_WAIT)
             click_on_top_interaction(i)
-            decline()
-    find_and_click_image_with_search(PRIMIS, 0, 0, "up")
+            process_application(False)
+    find_and_click_image(PRIMIS, 0, 0, "up")
     find_and_click_image("appletarget/personal_info.png")
     find_and_click_image("appletarget/marked_deceased.png")
 
 
-def confirm():
-    global noted_date, initials, full_date
+def process_application(is_confirmed=True):
+    global noted_date, initials, FULL_DATE, CRM_cords
 
     find_and_click_image("appletarget/tab_down_complete.png")
-    find_and_click_image("appletarget/completed_form.png")
-    find_and_click_image("appletarget/wait_for_complete.png")
-    tab_command(7, 0)
-    keyboard.write(full_date)
+    if is_confirmed:
+        find_and_click_image("appletarget/completed_form.png")
+        find_and_click_image("appletarget/wait_for_complete.png")
+    else:
+        find_and_click_image("appletarget/declined.png")
+        find_and_click_image("appletarget/wait_for_decline.png")
+
+    tab_command(7)
+    keyboard.write(FULL_DATE)
     pyperclip.copy("")
-    tab_command(3, 0)
+    tab_command(3)
     keyboard.press("command+A")
     time.sleep(0.1)
     keyboard.press_and_release("command+C")
@@ -279,63 +222,25 @@ def confirm():
     ):
         noted_date = pyautogui.prompt(text="", title="Noted Date?", default="1/")
         find_and_click_image("appletarget/sites.png")
-    if is_text_empty(found_text) == False:
-        pyautogui.press("down")
-        pyautogui.press("enter")
-        pyautogui.press("enter")
-    keyboard.write("Note: Not Researched - " + initials)
-    tab_command(2, 0)
-    pyautogui.press("enter")
 
-
-def decline():
-    global initials, CRM_cords, noted_date
-    find_and_click_image("appletarget/tab_down_complete.png")
-    find_and_click_image("appletarget/declined.png")
-    tab_command(7, 0)
-    keyboard.write(full_date)
-    pyperclip.copy("")
-    tab_command(3, 0)
-    keyboard.press("command+A")
-    time.sleep(0.1)
-    keyboard.press_and_release("command+C")
-    found_text = pyperclip.paste()
-    if (
-        (
-            extract_digits_from_text(found_text) != ""
-            or "year" in found_text
-            or "month" in found_text
-            or "January" in found_text
-            or "February" in found_text
-            or "March" in found_text
-            or "April" in found_text
-            or "May" in found_text
-            or "June" in found_text
-            or "July" in found_text
-            or "August" in found_text
-            or "September" in found_text
-            or "October" in found_text
-            or "November" in found_text
-            or "December" in found_text
-        )
-        and "id=" not in found_text
-        and "batch" not in found_text
-    ):
-        noted_date = pyautogui.prompt(text="", title="Noted Date?", default="1/")
-        find_and_click_image("appletarget/sites.png")
     pyautogui.press("down")
     pyautogui.press("enter")
     pyautogui.press("enter")
-    keyboard.write("Note: Duplicate - " + initials)
-    tab_command(2, 0)
+
+    if is_confirmed:
+        keyboard.write("Note: Not Researched - " + initials)
+    else:
+        keyboard.write("Note: Duplicate - " + initials)
+
+    tab_command(2)
     pyautogui.press("enter")
 
 
 def deceased_form():
-    global noted_date, formatted_date
+    global noted_date, FORMATTED_DATE
     find_and_click_image("appletarget/deceased_date.png")
     if noted_date == "1/":
-        keyboard.write(formatted_date)
+        keyboard.write(FORMATTED_DATE)
     elif noted_date != "1/":
         keyboard.write(noted_date)
         noted_date = "1/"
@@ -349,11 +254,11 @@ def move_to_communications():
     find_and_click_image("appletarget/constitute.png")
     find_and_click_image(PRIMIS)  # TODO find a way to remove
     find_and_click_image("appletarget/communications.png")
-    find_and_click_image_with_search("appletarget/add.png", 0, 0, "down")
+    find_and_click_image("appletarget/add.png", 0, 0, "down")
 
 
 def opt_out_form():
-    global full_date
+    global FULL_DATE
     find_and_click_image("appletarget/solicit_code.png")
     keyboard.write("Imprimis")
     find_and_click_image("appletarget/imprimis_three.png")
@@ -364,14 +269,14 @@ def opt_out_form():
     keyboard.write("Opt-out")
     find_and_click_image("appletarget/opt_out.png")
     pyautogui.press("tab")
-    keyboard.write(full_date)
+    keyboard.write(FULL_DATE)
     find_and_click_image("appletarget/source_file_tab_down.png")
     find_and_click_image("appletarget/double_deceased.png")
     pyautogui.press("enter")
 
 
 def end_time_recording(start_time):
-    global full_date
+    global FULL_DATE
     end_time = time.time()
     duration = end_time - start_time
     log_file = "program_log.txt"
@@ -380,35 +285,22 @@ def end_time_recording(start_time):
 
 
 def cutoff_section_of_screen(image_filename):
-    # find the top y coordinate of the image on the screen
-    global DELAY, MAX_ATTEMPTS, x_scale, y_scale, CONFIDENCE
+    global delay, MAX_ATTEMPTS, x_scale, y_scale, confidence
     box = None
     attempts = 0
     while box is None:
         box = pyautogui.locateOnScreen(
             image_filename,
-            confidence=CONFIDENCE,
+            confidence=confidence,
             region=(0, 0, round(2880 * x_scale), round(1800 * y_scale)),
         )
-        time.sleep(DELAY * 5)
+        time.sleep(delay * 5)
         # print("Searching for image: " + image_filename)
         attempts += 1
         if attempts >= MAX_ATTEMPTS:
-            if os.name == "posix":  # macOS
-                os.system(
-                    f'osascript -e \'display notification "Could not find image {image_filename}" with title "Error"\''
-                )
-            elif os.name == "nt":  # Windows
-                os.system(
-                    f'powershell -command "New-BurntToastNotification -Text "Could not find image {image_filename}" -AppLogo '
-                    + '"'
-                    + os.getcwd()
-                    + "/appletarget/chrome.png"
-                    + ")"
-                    + '"'
-                )
-            else:
-                print("Unsupported operating system")
+            os.system(
+                f'osascript -e \'display notification "Could not find image {image_filename}" with title "Error"\''
+            )
             break
 
     _, y, width, height = box
@@ -419,16 +311,17 @@ def cutoff_section_of_screen(image_filename):
 
 
 def main():
-    global initials, cutOffTopY, x_scale, y_scale, CRM_cords, cutOffBottomY, EDUCATION, COM_NUM
+    global initials, cutOffTopY, x_scale, y_scale, CRM_cords, cutOffBottomY, EDUCATION, COM_NUM, delay
     input_str = pyautogui.prompt(
-        text="Enter Initials and which computer number this is, -1 to quit",
-        title="Enter Initials and which computer number this is, -1 to quit",
-        default="DE, 1",
+        text="Enter Initials, which computer number this is, and delay time -1 to quit",
+        title="Enter Initials, which computer number this is, and delay time -1 to quit",
+        default="DE, 1, 0.1",
     )
-    initials, computer_number = input_str.strip().split(",")
+    initials, computer_number, delay = input_str.strip().split(",")
     screen_width, screen_height = pyautogui.size()
     x_scale = screen_width / 1440
     y_scale = screen_height / 900
+    delay = float(delay.strip())
     COM_NUM = int(computer_number.strip())
     cutOffBottomY = screen_height
     cutOffTopY, CRM_cords = cutoff_section_of_screen("appletarget/blackbaud_CRM.png")
@@ -438,8 +331,8 @@ def main():
         start_time = time.time()
 
         get_to_dead_page()
-        num = interactions_num_finder()
-        interactions_section(num)
+        number_of_interactions = interactions_num_finder()
+        interactions_section(number_of_interactions)
         deceased_form()
         move_to_communications()
         opt_out_form()

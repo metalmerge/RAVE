3e923fe792d9944514062e374c0bec9d45e0dacb
Metalmerge <ermakovd06@gmail.com>
2023-10-04 18:47:18 -0400

    windows beta

--- windows_main.py
+++ windows_main.py
@@ -6,7 +6,6 @@ import pyperclip
 import keyboard
 import pyautogui
 from pytesseract import pytesseract
-import os
 
 # Interactions: 2 = 79.14 bot; 75.66 no copy pasting, experienced, fast as possible human
 # Interactions: 1 = 33.86 bot; 45.68 human
@@ -24,6 +23,16 @@ import os
 # original_y_scale = 900 / 1800
 # make sure commands and control are accounted for based on type of computer
 
+x_scale = 1
+y_scale = 1
+COM_NUM = 1
+delay = 0.05
+confidence = 0.7
+CRM_cords = (0, 0)
+cutOffTopY = 0
+cutOffBottomY = 900
+pyautogui.FAILSAFE = True
+pyautogui.PAUSE = delay
 DEFAULT_PROMPT = "0"
 initials = "DE"
 noted_date = "1/"
@@ -31,120 +40,50 @@ PRIMIS = "windowstarget/receives_imprimis.png"
 EDUCATION = "windowstarget/education.png"
 LOAD_OPT_OUT_WAIT = "windowstarget/wait_for_load_opt_out.png"
 LOAD_OWNER_WAIT = "windowstarget/wait_for_load_owner.png"
-DELAY = 0.05
-MAX_ATTEMPTS = round(1.25 / (DELAY * 5))
-x_scale = 1
-y_scale = 1
-COM_NUM = 1
-CONFIDENCE = 0.7
-pyautogui.FAILSAFE = True
-pyautogui.PAUSE = DELAY
-current_date = datetime.now()
-
-formatted_date = current_date.strftime("%m/%Y")
-full_date = current_date.strftime("%m/%d/%Y")
-
-CRM_cords = (0, 0)
-cutOffTopY = 0
-cutOffBottomY = 900
+PRIMARY_EMAIL = "windowstarget/primary_email.png"
+MAX_ATTEMPTS = round(1.25 / (delay * 5))
+CURRENT_DATE = datetime.now()
+FORMATTED_DATE = CURRENT_DATE.strftime("%m/%Y")
+FULL_DATE = CURRENT_DATE.strftime("%m/%d/%Y")
 
 
-def find_and_click_image_with_search(image_filename, biasx, biasy, up_or_down):
-    global cutOffTopY, DELAY, MAX_ATTEMPTS, x_scale, y_scale, cutOffBottomY, CONFIDENCE
+def find_and_click_image(image_filename, biasx=0, biasy=0, up_or_down=None):
+    global cutOffTopY, delay, MAX_ATTEMPTS, x_scale, y_scale, cutOffBottomY, confidence, PRIMARY_EMAIL
     box = None
     print("Searching for image: " + image_filename)
+
+    if image_filename == "windowstarget/source_file_tab_down.png":
+        confidence = 0.8
+
     while box is None:
         box = pyautogui.locateOnScreen(
             image_filename,
-            confidence=CONFIDENCE,
+            confidence=confidence,
             region=(
                 0,
                 cutOffTopY,
                 round(2880 * x_scale),
-                round(cutOffBottomY * 2 * y_scale),  # theocratically this works
+                round(cutOffBottomY * 2 * y_scale),  # Theoretically this works
             ),
         )
-        time.sleep(DELAY * 10)
-        if box is None:
-            factor = -14
-            if up_or_down == "up":
-                factor = 14
+        time.sleep(delay * 5)
+
+        if box is None and up_or_down:
+            factor = -14 if up_or_down == "up" else 14
             pyautogui.scroll(factor)
-            time.sleep(DELAY * 2)
+            time.sleep(delay * 2)
 
     x, y, width, height = box
     x = box.left + width / 2 + biasx
     y = box.top + height / 2 + biasy
-    if (  # TODO find a way to remove
-        image_filename != PRIMIS
-        and image_filename != EDUCATION
-        and image_filename != "windowstarget/primary_email.png"
-        and image_filename != LOAD_OPT_OUT_WAIT
-        and image_filename != LOAD_OWNER_WAIT
-    ):
-        cord_click((x, y))
 
-
-def find_and_click_image_with_bias(image_filename, biasx, biasy):
-    global cutOffTopY, DELAY, MAX_ATTEMPTS, x_scale, y_scale, cutOffBottomY, CONFIDENCE
-    box = None
-    print("Searching for image: " + image_filename)
-    while box is None:
-        box = pyautogui.locateOnScreen(
-            image_filename,
-            confidence=CONFIDENCE,
-            region=(
-                0,
-                cutOffTopY,
-                round(2880 * x_scale),
-                round(cutOffBottomY * 2 * y_scale),  # theocratically this works
-            ),
-        )
-        time.sleep(DELAY * 5)
-
-    x, y, width, height = box
-    x = box.left + width / 2 + biasx
-    y = box.top + height / 2 + biasy
-    if (  # TODO find a way to remove
-        image_filename != PRIMIS
-        and image_filename != EDUCATION
-        and image_filename != "windowstarget/primary_email.png"
-        and image_filename != LOAD_OPT_OUT_WAIT
-        and image_filename != LOAD_OWNER_WAIT
-    ):
-        cord_click((x, y))
-
-
-def find_and_click_image(image_filename):
-    global cutOffTopY, DELAY, MAX_ATTEMPTS, x_scale, y_scale, cutOffBottomY, CONFIDENCE
-    box = None
-
-    print("Searching for image: " + image_filename)
-    if (image_filename == "windowstarget/source_file_tab_down.png"):
-        CONFIDENCE = .8
-    while box is None:
-        box = pyautogui.locateOnScreen(
-            image_filename,
-            confidence=CONFIDENCE,
-            region=(
-                0,
-                cutOffTopY,
-                round(2880 * x_scale),
-                round(cutOffBottomY * 2 * y_scale),  # theocratically this works
-            ),
-        )
-        time.sleep(DELAY * 5)
-
-    x, y, width, height = box
-    x = box.left + width / 2
-    y = box.top + height / 2
-    if (  # TODO find a way to remove
-        image_filename != PRIMIS
-        and image_filename != EDUCATION
-        and image_filename != LOAD_OPT_OUT_WAIT
-        and image_filename != "windowstarget/primary_email.png"
-        and image_filename != LOAD_OWNER_WAIT
-    ):
+    if image_filename not in [
+        PRIMIS,
+        EDUCATION,
+        PRIMARY_EMAIL,
+        LOAD_OPT_OUT_WAIT,
+        LOAD_OWNER_WAIT,
+    ]:
         cord_click((x, y))
 
 
@@ -187,11 +126,11 @@ def get_to_dead_page():
         find_and_click_image("windowstarget/third_page.png")
     if COM_NUM == 3:
         find_and_click_image("windowstarget/fifth_page.png")
-    find_and_click_image_with_bias("windowstarget/name.png", 0, round(25 * y_scale))
+    find_and_click_image("windowstarget/name.png", 0, round(25 * y_scale))
 
 
 def interactions_num_finder():
-    global DELAY
+    global delay
     while True:
         pretext = "Interactions: "
         try:
@@ -202,13 +141,12 @@ def interactions_num_finder():
                 524,
             )
             if pretext in text:
-                # Extract the number following "Interactions:"
                 num_index = text.index(pretext) + len(pretext)
                 num_text = text[num_index:].strip()
                 num = int(extract_digits_from_text(num_text))
                 break
             else:
-                time.sleep(DELAY)
+                time.sleep(delay)
                 continue
         except ValueError:
             continue
@@ -216,38 +154,35 @@ def interactions_num_finder():
 
 
 def click_on_top_interaction(num):
-    find_and_click_image_with_search(
+    find_and_click_image(
         "windowstarget/status_alone.png", 0, round(num * 30 * y_scale), "down"
     )
-    # time.sleep(1)
-    find_and_click_image_with_search("windowstarget/edit_interaction.png", 0, 0, "down")
+    find_and_click_image("windowstarget/edit_interaction.png", 0, 0, "down")
 
 
 def interactions_section(num):
-    global PRIMIS, LOAD_OWNER_WAIT
+    global PRIMIS, LOAD_OWNER_WAIT, PRIMARY_EMAIL
     find_and_click_image("windowstarget/interactions.png")
     click_on_top_interaction(1)
     confirm()
-    if num > 1:  # untested
-        for i in range(2, num+1):
+    if num > 1:
+        for i in range(2, num + 1):
             find_and_click_image(LOAD_OWNER_WAIT)
             click_on_top_interaction(i)
             find_and_click_image(LOAD_OWNER_WAIT)
             decline()
-    # time.sleep(2)
-    find_and_click_image_with_search("windowstarget/primary_email.png", 0, 0, "up")
+    find_and_click_image(PRIMARY_EMAIL, 0, 0, "up")
     find_and_click_image("windowstarget/personal_info.png")
     find_and_click_image("windowstarget/marked_deceased.png")
 
 
 def confirm():
-    global noted_date, initials, full_date
-
+    global noted_date, initials, FULL_DATE
     find_and_click_image("windowstarget/tab_down_complete.png")
     find_and_click_image("windowstarget/completed_form.png")
     find_and_click_image("windowstarget/wait_for_complete.png")
     tab_command(9, 0)
-    keyboard.write(full_date)
+    keyboard.write(FULL_DATE)
     # pyperclip.copy("")
     tab_command(3, 0)
     # keyboard.press("ctrl+a")
@@ -256,7 +191,7 @@ def confirm():
     # keyboard.write("Note: Confirmed - " + initials)
     # keyboard.press("ctrl+v")
     # found_text = pyperclip.paste()
-    time.sleep(DELAY)
+    time.sleep(delay)
     found_text = extract_text_from_coordinates(601, 600, 1277, 726)
     if (
         (
@@ -281,7 +216,7 @@ def confirm():
     ):
         noted_date = pyautogui.prompt(text="", title="Noted Date?", default="1/")
         find_and_click_image("windowstarget/sites.png")
-        tab_command(2,0)
+        tab_command(2, 0)
     if is_text_empty(found_text) == False:
         pyautogui.press("down")
         pyautogui.press("enter")
@@ -297,14 +232,14 @@ def decline():
     find_and_click_image("windowstarget/declined.png")
     find_and_click_image("windowstarget/wait_for_declined.png")
     tab_command(8, 0)
-    keyboard.write(full_date)
+    keyboard.write(FULL_DATE)
     # pyperclip.copy("")
     tab_command(3, 0)
     # keyboard.press("command+A")
     # time.sleep(0.1)
     # keyboard.press_and_release("command+C")
     # found_text = pyperclip.paste()
-    time.sleep(DELAY)
+    time.sleep(delay)
     found_text = extract_text_from_coordinates(601, 600, 1277, 726)
     if (
         (
@@ -339,10 +274,10 @@ def decline():
 
 
 def deceased_form():
-    global noted_date, formatted_date
+    global noted_date, FORMATTED_DATE
     find_and_click_image("windowstarget/deceased_date.png")
     if noted_date == "1/":
-        keyboard.write(formatted_date)
+        keyboard.write(FORMATTED_DATE)
     elif noted_date != "1/":
         keyboard.write(noted_date)
         noted_date = "1/"
@@ -360,7 +295,7 @@ def move_to_communications():
 
 
 def opt_out_form():
-    global full_date
+    global FULL_DATE
     find_and_click_image("windowstarget/solicit_code.png")
     keyboard.write("Imprimis")
     find_and_click_image("windowstarget/imprimis_three.png")
@@ -372,17 +307,14 @@ def opt_out_form():
     keyboard.write("Opt-out")
     find_and_click_image("windowstarget/opt_out.png")
     pyautogui.press("tab")
-    keyboard.write(full_date)
+    keyboard.write(FULL_DATE)
     find_and_click_image("windowstarget/source_file_tab_down.png")
-    # tab_command(3,0)
-    # keyboard.write("Deceased")
     find_and_click_image("windowstarget/double_deceased.png")
-    # tab_command(5,0)
     pyautogui.press("enter")
 
 
 def end_time_recording(start_time):
-    global full_date
+    global FULL_DATE
     end_time = time.time()
     duration = end_time - start_time
     log_file = "program_log.txt"
@@ -392,15 +324,15 @@ def end_time_recording(start_time):
 
 def cutoff_section_of_screen(image_filename):
     # find the top y coordinate of the image on the screen
-    global DELAY, MAX_ATTEMPTS, x_scale, y_scale, CONFIDENCE
+    global delay, MAX_ATTEMPTS, x_scale, y_scale, confidence
     box = None
     while box is None:
         box = pyautogui.locateOnScreen(
             image_filename,
-            confidence=CONFIDENCE,
+            confidence=confidence,
             region=(0, 0, round(2880 * x_scale), round(1800 * y_scale)),
         )
-        time.sleep(DELAY * 5)
+        time.sleep(delay * 5)
         print("Searching for image: " + image_filename)
 
     _, y, width, height = box
@@ -412,16 +344,17 @@ def cutoff_section_of_screen(image_filename):
 
 
 def main():
-    global initials, cutOffTopY, x_scale, y_scale, CRM_cords, cutOffBottomY, EDUCATION, COM_NUM
+    global initials, cutOffTopY, x_scale, y_scale, CRM_cords, cutOffBottomY, EDUCATION, COM_NUM, delay, PRIMARY_EMAIL
     input_str = pyautogui.prompt(
-        text="Enter Initials and which computer number this is, -1 to quit",
-        title="Enter Initials and which computer number this is, -1 to quit",
-        default="DE, 1",
+        text="Enter Initials, which computer number this is, and delay time -1 to quit",
+        title="Enter Initials, which computer number this is, and delay time -1 to quit",
+        default="DE, 1, 0.1",
     )
-    initials, computer_number = input_str.strip().split(",")
+    initials, computer_number, delay = input_str.strip().split(",")
     screen_width, screen_height = pyautogui.size()
     x_scale = screen_width / 1440
     y_scale = screen_height / 900
+    delay = float(delay.strip())
     COM_NUM = int(computer_number.strip())
     cutOffBottomY = screen_height
     cutOffTopY, CRM_cords = cutoff_section_of_screen("windowstarget/blackbaud_CRM.png")
@@ -430,14 +363,16 @@ def main():
         start_time = time.time()
         get_to_dead_page()
         num = interactions_num_finder()
-        
+
         interactions_section(num)
         deceased_form()
         move_to_communications()
         opt_out_form()
 
         end_time_recording(start_time)
-        find_and_click_image("windowstarget/primary_email.png")  # TODO find a way to remove
+        find_and_click_image(PRIMARY_EMAIL)  # TODO find a way to remove
+        time.sleep(2)
+
 
 if __name__ == "__main__":
     main()

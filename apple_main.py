481d79d41d462afa9380c7424bd13415d63788e1
Metalmerge <ermakovd06@gmail.com>
2023-10-07 14:47:46 -0400

    spelling fixes

--- apple_main.py
+++ apple_main.py
@@ -17,10 +17,10 @@ from main_shared_functions import (
 )
 
 # TODO
-# Protential improvements:
+# Potential improvements:
 #   - Find a better way to know the number of interactions than using extract_text_from_coordinates
 #   - Use mss to take screenshots instead of pyautogui
-#   - Find workaround to recieves imprints and waits
+#   - Find workaround to receives imprints and waits
 
 # original_x_scale = 1440 / 2880
 # original_y_scale = 900 / 1800
@@ -41,10 +41,10 @@ CURRENT_DATE = datetime.now()
 FORMATTED_DATE = CURRENT_DATE.strftime("%-m/%Y")
 FULL_DATE = CURRENT_DATE.strftime("%-m/%-d/%Y")
 DEFAULT_PROMPT = "0"
-PRIMIS = "appletarget/receives_imprimis.png"
-EDUCATION = "appletarget/education.png"
-LOAD_OPT_OUT_WAIT = "appletarget/wait_for_load_opt_out.png"
-LOAD_OWNER_WAIT = "appletarget/wait_for_load_owner.png"
+IMPRIMIS = "appleTarget/receives_imprimis.png"
+EDUCATION = "appleTarget/education.png"
+LOAD_OPT_OUT_WAIT = "appleTarget/wait_for_load_opt_out.png"
+LOAD_OWNER_WAIT = "appleTarget/wait_for_load_owner.png"
 MAX_ATTEMPTS = round(1.25 / (delay * 5))
 
 
@@ -92,7 +92,7 @@ def find_and_click_image(image_filename, biasx=0, biasy=0, up_or_down=None):
 
     # Click on the found image if it's not in the specified list
     if image_filename not in [
-        PRIMIS,
+        IMPRIMIS,
         EDUCATION,
         LOAD_OPT_OUT_WAIT,
         LOAD_OWNER_WAIT,
@@ -105,17 +105,17 @@ def get_to_dead_page():
     global COM_NUM
 
     # Click on "constituents" and "updates" images
-    find_and_click_image("appletarget/constituents.png")
-    find_and_click_image("appletarget/updates.png")
+    find_and_click_image("appleTarget/constituents.png")
+    find_and_click_image("appleTarget/updates.png")
 
     # Check the value of COM_NUM to determine further actions
     if COM_NUM == 2:
-        find_and_click_image("appletarget/third_page.png")
+        find_and_click_image("appleTarget/third_page.png")
     if COM_NUM == 3:
-        find_and_click_image("appletarget/fifth_page.png")
+        find_and_click_image("appleTarget/fifth_page.png")
 
     # Click on the "name" image with a vertical bias
-    find_and_click_image("appletarget/name.png", 0, round(25 * y_scale))
+    find_and_click_image("appleTarget/name.png", 0, round(25 * y_scale))
 
 
 def interactions_num_finder():
@@ -149,20 +149,20 @@ def interactions_num_finder():
 def click_on_top_interaction(number_of_interactions):
     # Click on the "status_alone" image and "edit_interaction" image
     find_and_click_image(
-        "appletarget/status_alone.png",
+        "appleTarget/status_alone.png",
         0,
         round(number_of_interactions * 30 * y_scale),
         "down",
     )
-    find_and_click_image("appletarget/edit_interaction.png", 0, 0, "down")
+    find_and_click_image("appleTarget/edit_interaction.png", 0, 0, "down")
 
 
 def interactions_section(number_of_interactions):
     # Global variables used in this method
-    global PRIMIS, LOAD_OWNER_WAIT
+    global IMPRIMIS, LOAD_OWNER_WAIT
 
     # Click on the "interactions" image
-    find_and_click_image("appletarget/interactions.png")
+    find_and_click_image("appleTarget/interactions.png")
 
     # Click on the top interaction
     click_on_top_interaction(1)
@@ -177,10 +177,10 @@ def interactions_section(number_of_interactions):
             # Process the application for subsequent interactions (not confirmed)
             process_application(False)
 
-    # Click on PRIMIS, "personal_info," and "marked_deceased"
-    find_and_click_image(PRIMIS, 0, 0, "up")
-    find_and_click_image("appletarget/personal_info.png")
-    find_and_click_image("appletarget/marked_deceased.png")
+    # Click on IMPRIMIS, "personal_info," and "marked_deceased"
+    find_and_click_image(IMPRIMIS, 0, 0, "up")
+    find_and_click_image("appleTarget/personal_info.png")
+    find_and_click_image("appleTarget/marked_deceased.png")
 
 
 def process_application(is_confirmed=True):
@@ -188,16 +188,16 @@ def process_application(is_confirmed=True):
     global noted_date, initials, FULL_DATE, CRM_cords
 
     # Click on "tab_down_complete" image
-    find_and_click_image("appletarget/tab_down_complete.png")
+    find_and_click_image("appleTarget/tab_down_complete.png")
 
     if is_confirmed:
         # If confirmed, click on "completed_form" and "wait_for_complete" images
-        find_and_click_image("appletarget/completed_form.png")
-        find_and_click_image("appletarget/wait_for_complete.png")
+        find_and_click_image("appleTarget/completed_form.png")
+        find_and_click_image("appleTarget/wait_for_complete.png")
     else:
         # If not confirmed, click on "declined" and "wait_for_decline" images
-        find_and_click_image("appletarget/declined.png")
-        find_and_click_image("appletarget/wait_for_decline.png")
+        find_and_click_image("appleTarget/declined.png")
+        find_and_click_image("appleTarget/wait_for_decline.png")
 
     # Press tab 7 times
     tab_command(7)
@@ -237,7 +237,7 @@ def process_application(is_confirmed=True):
         noted_date = pyautogui.prompt(
             text="", title="Noted Date?", default=detect_dates(found_text)
         )
-        find_and_click_image("appletarget/sites.png")
+        find_and_click_image("appleTarget/sites.png")
 
     # Press down, enter, and enter keys
     pyautogui.press("down")
@@ -259,7 +259,7 @@ def deceased_form():
     global noted_date, FORMATTED_DATE
 
     # Click on the "deceased_date" image
-    find_and_click_image("appletarget/deceased_date.png")
+    find_and_click_image("appleTarget/deceased_date.png")
 
     # Check if noted_date is "1/" and write the appropriate date
     if noted_date == "1/":
@@ -269,20 +269,20 @@ def deceased_form():
         noted_date = "1/"
 
     # Click on "source_tab_down" and "communication_from," then press enter
-    find_and_click_image("appletarget/source_tab_down.png")
-    find_and_click_image("appletarget/communication_from.png")
+    find_and_click_image("appleTarget/source_tab_down.png")
+    find_and_click_image("appleTarget/communication_from.png")
     pyautogui.press("enter")
 
 
 def move_to_communications():
     # Global variables used in this method
-    global PRIMIS
+    global IMPRIMIS
 
-    # Click on "constitute," PRIMIS, "communications," and "add" (simulating a down key press)
-    find_and_click_image("appletarget/constitute.png")
-    find_and_click_image(PRIMIS)
-    find_and_click_image("appletarget/communications.png")
-    find_and_click_image("appletarget/add.png", 0, 0, "down")
+    # Click on "constitute," IMPRIMIS, "communications," and "add" (simulating a down key press)
+    find_and_click_image("appleTarget/constitute.png")
+    find_and_click_image(IMPRIMIS)
+    find_and_click_image("appleTarget/communications.png")
+    find_and_click_image("appleTarget/add.png", 0, 0, "down")
 
 
 def opt_out_form():
@@ -290,12 +290,12 @@ def opt_out_form():
     global FULL_DATE
 
     # Click on "solicit_code" image and write "Imprimis"
-    find_and_click_image("appletarget/solicit_code.png")
+    find_and_click_image("appleTarget/solicit_code.png")
     keyboard.write("Imprimis")
 
     # Click on "imprimis_three" and "imprimis_done" images
-    find_and_click_image("appletarget/imprimis_three.png")
-    find_and_click_image("appletarget/imprimis_done.png")
+    find_and_click_image("appleTarget/imprimis_three.png")
+    find_and_click_image("appleTarget/imprimis_done.png")
 
     # Press tab key
     pyautogui.press("tab")
@@ -308,13 +308,13 @@ def opt_out_form():
     keyboard.write("Opt-out")
 
     # Click on "opt_out" image and press tab
-    find_and_click_image("appletarget/opt_out.png")
+    find_and_click_image("appleTarget/opt_out.png")
     pyautogui.press("tab")
 
     # Write FULL_DATE and click on "source_file_tab_down" and "double_deceased," then press enter
     keyboard.write(FULL_DATE)
-    find_and_click_image("appletarget/source_file_tab_down.png")
-    find_and_click_image("appletarget/double_deceased.png")
+    find_and_click_image("appleTarget/source_file_tab_down.png")
+    find_and_click_image("appleTarget/double_deceased.png")
     pyautogui.press("enter")
 
 
@@ -388,7 +388,7 @@ def main():
     cutOffBottomY = screen_height
 
     # Calculate cutOffTopY and CRM_cords using the cutoff_section_of_screen method
-    cutOffTopY, CRM_cords = cutoff_section_of_screen("appletarget/blackbaud_CRM.png")
+    cutOffTopY, CRM_cords = cutoff_section_of_screen("appleTarget/blackbaud_CRM.png")
 
     # Click on CRM_cords
     cord_click(CRM_cords)

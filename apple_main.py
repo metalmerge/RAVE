33d2aa3eaa8c3df718cfb74dea8ae71220faba4b
Dimitry Ermakov <ermakovd06@gmail.com>
2023-10-07 19:27:40 +0000

    apple spacing fix

--- apple_main.py
+++ apple_main.py
@@ -49,10 +49,20 @@ MAX_ATTEMPTS = round(1.25 / (delay * 5))
 
 
 def find_and_click_image(image_filename, biasx=0, biasy=0, up_or_down=None):
-    # Initialize variables for locating the image
+    """
+    Locate an image on the screen and click on it.
+
+    Args:
+        image_filename (str): The filename of the image to locate and click.
+        biasx (int, optional): Horizontal bias to adjust the click position (default is 0).
+        biasy (int, optional): Vertical bias to adjust the click position (default is 0).
+        up_or_down (str, optional): Scroll the screen 'up' or 'down' if the image is not found (default is None).
+
+    Returns:
+        None
+    """
     box = None
     attempts = 0
-
     # Continue attempting to locate the image until found
     while box is None:
         # Attempt to locate the image on the screen within the specified region
@@ -66,19 +76,14 @@ def find_and_click_image(image_filename, biasx=0, biasy=0, up_or_down=None):
                 round(cutOffBottomY * 2 * y_scale),
             ),
         )
-
         # Sleep to avoid excessive attempts
         time.sleep(delay * 5)
-
-        # Increment the attempts count
         attempts += 1
-
         # Display an error notification if the maximum attempts are reached
         if attempts >= MAX_ATTEMPTS:
             os.system(
                 f'osascript -e \'display notification "Could not find image {image_filename}" with title "Error" sound name "Glass"\''
             )
-
         # If the image is still not found and up_or_down is provided, simulate key presses
         if box is None and up_or_down is not None:
             pyautogui.press(up_or_down)
@@ -89,7 +94,6 @@ def find_and_click_image(image_filename, biasx=0, biasy=0, up_or_down=None):
     x, y, width, height = box
     x = box.left / 2 + width / 4 + biasx
     y = box.top / 2 + height / 4 + biasy
-
     # Click on the found image if it's not in the specified list
     if image_filename not in [
         IMPRIMIS,
@@ -101,27 +105,20 @@ def find_and_click_image(image_filename, biasx=0, biasy=0, up_or_down=None):
 
 
 def get_to_dead_page():
-    # Global variable used in this method
     global COM_NUM
-
-    # Click on "constituents" and "updates" images
     find_and_click_image("appleTarget/constituents.png")
     find_and_click_image("appleTarget/updates.png")
-
     # Check the value of COM_NUM to determine further actions
     if COM_NUM == 2:
         find_and_click_image("appleTarget/third_page.png")
     if COM_NUM == 3:
         find_and_click_image("appleTarget/fifth_page.png")
-
     # Click on the "name" image with a vertical bias
     find_and_click_image("appleTarget/name.png", 0, round(25 * y_scale))
 
 
 def interactions_num_finder():
-    # Global variables used in this method
     global delay
-
     while True:
         pretext = "Interactions: "
         try:
@@ -142,12 +139,10 @@ def interactions_num_finder():
                 continue
         except ValueError:
             continue
-
     return number_of_interactions
 
 
 def click_on_top_interaction(number_of_interactions):
-    # Click on the "status_alone" image and "edit_interaction" image
     find_and_click_image(
         "appleTarget/status_alone.png",
         0,
@@ -158,50 +153,32 @@ def click_on_top_interaction(number_of_interactions):
 
 
 def interactions_section(number_of_interactions):
-    # Global variables used in this method
     global IMPRIMIS, LOAD_OWNER_WAIT
-
-    # Click on the "interactions" image
     find_and_click_image("appleTarget/interactions.png")
-
-    # Click on the top interaction
     click_on_top_interaction(1)
-
     # Process the application for the first interaction
     process_application()
-
     if number_of_interactions > 1:
         for i in range(2, number_of_interactions):
             find_and_click_image(LOAD_OWNER_WAIT)
             click_on_top_interaction(i)
             # Process the application for subsequent interactions (not confirmed)
             process_application(False)
-
-    # Click on IMPRIMIS, "personal_info," and "marked_deceased"
     find_and_click_image(IMPRIMIS, 0, 0, "up")
     find_and_click_image("appleTarget/personal_info.png")
     find_and_click_image("appleTarget/marked_deceased.png")
 
 
 def process_application(is_confirmed=True):
-    # Global variables used in this method
     global noted_date, initials, FULL_DATE
-
-    # Click on "tab_down_complete" image
     find_and_click_image("appleTarget/tab_down_complete.png")
-
     if is_confirmed:
-        # If confirmed, click on "completed_form" and "wait_for_complete" images
         find_and_click_image("appleTarget/completed_form.png")
         find_and_click_image("appleTarget/wait_for_complete.png")
     else:
-        # If not confirmed, click on "declined" and "wait_for_decline" images
         find_and_click_image("appleTarget/declined.png")
         find_and_click_image("appleTarget/wait_for_decline.png")
-
-    # Press tab 7 times
     tab_command(7)
-
     # Write FULL_DATE and copy it to clipboard
     keyboard.write(FULL_DATE)
     pyperclip.copy("")
@@ -210,7 +187,6 @@ def process_application(is_confirmed=True):
     time.sleep(0.1)
     keyboard.press_and_release("command+C")
     found_text = pyperclip.paste()
-
     # Check if the found text indicates a date
     if (
         (
@@ -238,47 +214,34 @@ def process_application(is_confirmed=True):
             text="", title="Noted Date?", default=detect_dates(found_text)
         )
         find_and_click_image("appleTarget/sites.png")
-
-    # Press down, enter, and enter keys
-    pyautogui.press("down")
-    pyautogui.press("enter")
-    pyautogui.press("enter")
-
-    # Write a note based on confirmation status (Not Researched or Duplicate)
+    if found_text != "":
+        pyautogui.press("down")
+        pyautogui.press("enter")
+        pyautogui.press("enter")
     if is_confirmed:
         keyboard.write("Note: Not Researched - " + initials)
     else:
         keyboard.write("Note: Duplicate - " + initials)
-
     tab_command(2)
     pyautogui.press("enter")
 
 
 def deceased_form():
-    # Global variables used in this method
     global noted_date, FORMATTED_DATE
-
-    # Click on the "deceased_date" image
     find_and_click_image("appleTarget/deceased_date.png")
-
     # Check if noted_date is "1/" and write the appropriate date
     if noted_date == "1/":
         keyboard.write(FORMATTED_DATE)
     elif noted_date != "1/":
         keyboard.write(noted_date)
         noted_date = "1/"
-
-    # Click on "source_tab_down" and "communication_from," then press enter
     find_and_click_image("appleTarget/source_tab_down.png")
     find_and_click_image("appleTarget/communication_from.png")
     pyautogui.press("enter")
 
 
 def move_to_communications():
-    # Global variables used in this method
     global IMPRIMIS
-
-    # Click on "constitute," IMPRIMIS, "communications," and "add" (simulating a down key press)
     find_and_click_image("appleTarget/constitute.png")
     find_and_click_image(IMPRIMIS)
     find_and_click_image("appleTarget/communications.png")
@@ -286,32 +249,18 @@ def move_to_communications():
 
 
 def opt_out_form():
-    # Global variables used in this method
     global FULL_DATE
-
-    # Click on "solicit_code" image and write "Imprimis"
     find_and_click_image("appleTarget/solicit_code.png")
     keyboard.write("Imprimis")
-
-    # Click on "imprimis_three" and "imprimis_done" images
     find_and_click_image("appleTarget/imprimis_three.png")
     find_and_click_image("appleTarget/imprimis_done.png")
-
-    # Press tab key
     pyautogui.press("tab")
-
     # Press backspace 11 times to clear the text field
     for _ in range(0, 11):
         pyautogui.press("backspace")
-
-    # Write "Opt-out"
     keyboard.write("Opt-out")
-
-    # Click on "opt_out" image and press tab
     find_and_click_image("appleTarget/opt_out.png")
     pyautogui.press("tab")
-
-    # Write FULL_DATE and click on "source_file_tab_down" and "double_deceased," then press enter
     keyboard.write(FULL_DATE)
     find_and_click_image("appleTarget/source_file_tab_down.png")
     find_and_click_image("appleTarget/double_deceased.png")
@@ -319,9 +268,7 @@ def opt_out_form():
 
 
 def end_time_recording(start_time):
-    # Global variables used in this method
     global FULL_DATE
-
     # Calculate the duration and write it to a log file
     end_time = time.time()
     duration = end_time - start_time
@@ -331,12 +278,10 @@ def end_time_recording(start_time):
 
 
 def cutoff_section_of_screen(image_filename):
-    # Global variables used in this method
     global delay, MAX_ATTEMPTS, x_scale, y_scale, confidence
 
     box = None
     attempts = 0
-
     while box is None:
         # Locate the image on the screen with specified region and confidence level
         box = pyautogui.locateOnScreen(
@@ -346,25 +291,20 @@ def cutoff_section_of_screen(image_filename):
         )
         time.sleep(delay * 5)
         attempts += 1
-
         # Display an error notification if image cannot be found after maximum attempts
         if attempts >= MAX_ATTEMPTS:
             os.system(
                 f'osascript -e \'display notification "Could not find image {image_filename}" with title "Error"\''
             )
             break
-
     _, y, width, height = box
     image_cords_x = box.left / 2 + width / 4
     image_cords_y = box.top / 2 + height / 4
-
     return round(y / 2), (image_cords_x, image_cords_y)
 
 
 def main():
-    # Global variables used in this method
     global initials, cutOffTopY, x_scale, y_scale, CRM_cords, cutOffBottomY, EDUCATION, COM_NUM, delay
-
     # Prompt the user for input: initials, computer number, and delay time
     input_str = pyautogui.prompt(
         text="Enter Initials, which computer number this is, and delay time -1 to quit",
@@ -372,47 +312,32 @@ def main():
         default="DE, 1, 0.01",
     )
     initials, computer_number, delay = input_str.strip().split(",")
-
     # Get the screen width and height
     screen_width, screen_height = pyautogui.size()
-
     # Calculate the x and y scale factors based on the screen size
     x_scale = screen_width / 1440
     y_scale = screen_height / 900
-
     # Parse the delay and computer number
     delay = float(delay.strip())
     COM_NUM = int(computer_number.strip())
-
     # Set cutOffBottomY to screen height
     cutOffBottomY = screen_height
-
     # Calculate cutOffTopY and CRM_cords using the cutoff_section_of_screen method
     cutOffTopY, CRM_cords = cutoff_section_of_screen("appleTarget/blackbaud_CRM.png")
-
-    # Click on CRM_cords
     cord_click(CRM_cords)
-
-    # Main loop, continue until initials are "-1"
     while initials != "-1":
         start_time = time.time()
-
         # Navigate to the dead page
         get_to_dead_page()
-
         # Find the number of interactions
         number_of_interactions = interactions_num_finder()
-
         # Process interactions
         interactions_section(number_of_interactions)
-
         # Fill out the deceased form
         deceased_form()
-
         # Move to communications and fill out the opt-out form
         move_to_communications()
         opt_out_form()
-
         # Record end time and find_and_click_image for EDUCATION
         end_time_recording(start_time)
         find_and_click_image(EDUCATION)

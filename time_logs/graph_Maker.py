ec2bad98a590d68aa65b6d961dad9c8692212f6b
Metalmerge <ermakovd06@gmail.com>
2023-11-08 11:34:39 -0500

    Adjust outlier threshold and add moving average trend line to graph

--- time_logs/graph_Maker.py
+++ time_logs/graph_Maker.py
@@ -1,9 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy import stats
 
 # Define a threshold to ignore massive outliers
-outlier_threshold = 100.0  # You can adjust this threshold as needed
+outlier_threshold = 60.0  # You can adjust this threshold as needed
 
 # Read the time entries from the file and filter out outliers
 with open("windows_program_log.txt", "r") as file:
@@ -13,6 +12,9 @@ with open("windows_program_log.txt", "r") as file:
         if float(line.strip()) <= outlier_threshold
     ]
 
+# Limit the graph to show only the most recent 100 entries
+time_entries = time_entries[-100:]
+
 # Calculate statistics
 average_time = np.mean(time_entries)
 median_time = np.median(time_entries)
@@ -20,10 +22,22 @@ std_deviation = np.std(time_entries)
 
 # Create a plot
 plt.figure(figsize=(10, 6))  # You can adjust the figure size as needed
-plt.plot(time_entries, marker="o", linestyle="-")
-plt.title("Windows Program Log (Outliers Ignored)")
+
+# Add a moving average trend line (e.g., 5-point moving average)
+window_size = 5
+moving_average = np.convolve(
+    time_entries, np.ones(window_size) / window_size, mode="valid"
+)
+moving_average = np.pad(
+    moving_average, (window_size - 1, 0), "constant", constant_values=np.nan
+)
+
+plt.plot(time_entries, marker="o", linestyle="-", label="Time Entries")
+plt.plot(moving_average, linestyle="--", label=f"{window_size}-Point Moving Average")
+plt.title("Windows Program Log (Outliers Ignored, Most Recent 100 Entries)")
 plt.xlabel("Entry Number")
 plt.ylabel("Time (seconds)")
+plt.legend()
 
 # Save the plot as a PNG file
 plt.savefig("windows_program_log_filtered.png")
@@ -32,7 +46,7 @@ plt.savefig("windows_program_log_filtered.png")
 plt.show()
 
 # Display statistics
-print("Statistics for time entries:")
+print("Statistics for the most recent 100 time entries:")
 print(f"Average: {average_time:.2f} seconds")
 print(f"Median: {median_time:.2f} seconds")
 print(f"Standard Deviation: {std_deviation:.2f} seconds")
